8d00d8f6105b4fbf422c4608255ed4214483bb67
Raolis <raolis1989@gmail.com>
2021-02-22 17:05:03 -0300

    add-icon

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -8,13 +8,53 @@ import FavoritesStack from './FavoritesStack';
 import TopRestaurantsStack from './TopRestaurantsStack';
 import SearchStack from './SearchStack';
 import AccountStack from './AccountStack';
+import { Icon } from 'react-native-elements';
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
+    const screenOptions = (route , color) => {
+        let iconName;
+        switch (route.name){
+            case "restaurants":
+                iconName ="compass-outline"
+                break;
+          case "favorites":
+                iconName ="heart-outline"
+                break;  
+           case "top-restaurants":
+                iconName ="star-outline"
+                break;   
+           case "search":
+                iconName ="magnify"
+                break;
+           case "account":
+                iconName ="home-outline"
+                break;    
+        }
+
+        return (
+            <Icon
+                type="material-community"
+                name= {iconName}
+                size={22}
+                color={color}
+            />
+        )
+    }
+
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                initialRouteName="restaurants"
+                tabBarOptions={{
+                    inactiveTintColor:"#d8b45c",
+                    activeTintColor :"#6cbc94"
+                }}
+                screenOptions ={({route})=>({
+                    tabBarIcon :({color})=> screenOptions(route, color)
+                })}
+            >
                 <Tab.Screen
                     name ="restaurants"
                     component= {RestaurantsStack}
