f20c9eb3f30d5d32e371d56d98c50a59ce089d1f
Raolis <raolis1989@gmail.com>
2021-02-24 10:11:19 -0300

    styleuser-view

--- screens/account/Account.js
+++ screens/account/Account.js
@@ -1,7 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import  firebase from 'firebase';
-import { firebaseApp } from '../../utils/firebase'
+import { StyleSheet} from 'react-native'
 
 import UserGuest from './UserGuest';
 import UserLogged from './UserLogged';
--- screens/account/UserGuest.js
+++ screens/account/UserGuest.js
@@ -1,12 +1,53 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
+import {Button} from 'react-native-elements';
+
 
 export default function UserGuest() {
     return (
-        <View>
-            <Text>UserGuest..</Text>
-        </View>
+        <ScrollView
+            centerContent
+            style={styles.viewBody}
+        >
+            <Image
+                source={require("../../assets/logo.png")}
+                resizeMode="contain"
+                style={styles.image}
+            />
+            <Text style={styles.title}>Consulta tu perfil en Restaurants</Text>
+            <Text style={styles.description}>Busca y visualiza los mejores restaurantes de una forma 
+                sencilla, vota cual te ha gustado mas y comenta como ha sido tu experiencia.</Text>
+            <Button
+            buttonStyle={styles.button}
+            title="Ver tu perfil"
+            onPress={()=> console.log("Click!!")}/>
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    viewBody: {
+        marginHorizontal:50
+    },
+    image:{
+        height:300,
+        width:"100%",
+        marginBottom: 10,
+        textAlign:"center"
+    },
+    title:{
+        fontWeight:"bold",
+        fontSize: 19,
+        marginBottom: 10,
+        textAlign:"center"
+    },
+    description:{
+        textAlign:"justify",
+        marginBottom:20,
+        color:"#ffb41c"
+    }, 
+    button:{
+        backgroundColor: "#6cbc94"
+    }
+})
