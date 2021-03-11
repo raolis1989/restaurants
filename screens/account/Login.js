df553f83571678897340c0cb176c875810234790
Raolis <raolis1989@gmail.com>
2021-03-11 12:52:59 -0300

    screen-register

--- screens/account/Login.js
+++ screens/account/Login.js
@@ -1,12 +1,62 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native'
+import { Divider } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Login() {
     return (
-        <View>
-            <Text>Login</Text>
-        </View>
+        <ScrollView>
+            <Image
+             source={require("../../assets/logo.png")}
+             resizeMode="contain"
+             style={styles.image}
+            />
+            <View style={styles.container}>
+                <Text>Login</Text>
+                <CreateAccount/>
+            </View>
+            <Divider style={styles.divider}/>
+        </ScrollView>
+    )
+}
+function CreateAccount(props){
+    const navigation = useNavigation()
+    return (
+        <Text 
+            style={styles.register}
+            onPress={() =>navigation.navigate("register")}
+        >
+            No tienes cuenta?{" "} 
+            <Text style={styles.linkRegister}>
+                 Registrate
+            </Text>
+        </Text>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image:{
+        height:150,
+        width:"100%",
+        marginBottom:20
+    },
+    container: {
+        marginHorizontal:40
+    },
+    divider:{
+        backgroundColor: "#d8b45c",
+        margin: 40
+    },
+    register:{
+        marginTop:15,
+        marginHorizontal:10,
+        alignSelf:"center"
+    },
+    linkRegister:{
+        color:"#6cbc94",
+        fontWeight:"bold"
+    }
+
+    
+
+})
---
+++ screens/account/Register.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
+import RegisterForm from '../../components/Account/RegisterForm'
+
+
+export default function Register() {
+    return (
+        <KeyboardAwareScrollView>
+            <Image
+             source={require("../../assets/logo.png")}
+             resizeMode="contain"
+             style={styles.image}
+            />
+            <RegisterForm/>
+        </KeyboardAwareScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    image:{
+        height:150,
+        width:"100%",
+        marginBottom:20
+    }
+})
